0ad87aed7b0602ce1cd70642cbe553d7a8cbb5b0
oneung <41810171+oneung@users.noreply.github.com>
2021-12-09 16:44:11 +0900

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,13 +1,10 @@
 # 기본 계산기 기능3가지
+
 def add(a, b):
 	return a+b
 
 def substract(a, b):
 	return a-b
 
-<<<<<<< HEAD
 def multiply(a, b):
-=======
-def multiply(a,b):
->>>>>>> ff50355bfa770dd1ced6d733e03b2345eb6dff02
 	return a*b
